9c8bdd0bc45b84de117f69b12d44bebca305c41b
SanEk <tt.worm524@gmail.com>
2026-02-09 15:53:07 +0200

    Add project files.

---
+++ Controllers/TrailerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc;
+
+namespace film.Controllers
+{
+    public class TrailerController : Controller
+    {
+        public IActionResult Index(string link)
+        {
+            ViewData["trailerLink"] = link;
+            return View();
+        }
+
+        public IActionResult RedirectMethod()
+        {
+            return Redirect("/filmTrailer/Index");
+        }
+    }
+}
---
+++ Models/Film.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace mvc
+{
+    public class Film
+    {
+        [DisplayName("ID")]
+        public int Id { get; set; }
+
+        [DisplayName("Название")]
+        public string? Name { get; set; }
+
+        [DisplayName("Режиссер")]
+        public string? Director { get; set; }
+
+        [DisplayName("Жанр")]
+        public string? Genre { get; set; }
+
+        [DisplayName("Год выпуска")]
+        public int Year { get; set; }
+
+        [DisplayName("Постер")]
+        public string? Poster { get; set; }
+
+        [DisplayName("Описание")]
+        public string? Description { get; set; }
+
+        [DisplayName("Ссылка на трейлер")]
+        public string? TrailerLink { get; set; }
+    }
+}
