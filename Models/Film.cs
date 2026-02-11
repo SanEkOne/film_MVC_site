845d2d1da8051723f7d889689da96bb05af96694
SanEk <tt.worm524@gmail.com>
2026-02-11 15:55:16 +0200

    Validation

--- Models/Film.cs
+++ Models/Film.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using film_site.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace mvc
 {
@@ -8,24 +10,34 @@ namespace mvc
         public int Id { get; set; }
 
         [DisplayName("Название")]
+        [Required(ErrorMessage = "Поле должно быть заполнено!")]
         public string? Name { get; set; }
 
         [DisplayName("Режиссер")]
+        [Required(ErrorMessage = "Поле должно быть заполнено!")]
         public string? Director { get; set; }
 
         [DisplayName("Жанр")]
+        [Required(ErrorMessage = "Поле должно быть заполнено!")]
         public string? Genre { get; set; }
 
         [DisplayName("Год выпуска")]
+        [Required(ErrorMessage = "Поле должно быть заполнено!")]
+        [Range(1895, 2033, ErrorMessage = "Не корректный год!")]
         public int Year { get; set; }
 
         [DisplayName("Постер")]
+        [Required(ErrorMessage = "Поле должно быть заполнено!")]
+        [ValidPosterLink]
         public string? Poster { get; set; }
 
         [DisplayName("Описание")]
+        [Required(ErrorMessage = "Поле должно быть заполнено!")]
         public string? Description { get; set; }
 
         [DisplayName("Ссылка на трейлер")]
+        [Required(ErrorMessage = "Поле должно быть заполнено!")]
+        [Url(ErrorMessage = "Введите корректный URL!")]
         public string? TrailerLink { get; set; }
     }
 }
---
+++ Validation/ValidPosterLinkAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace film_site.Validation
+{
+    public class ValidPosterLinkAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+                return ValidationResult.Success;
+
+            if (value is not string url)
+                return new ValidationResult("Некорректный формат ссылки.");
+
+            if (!Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult) ||
+                (uriResult.Scheme != Uri.UriSchemeHttp &&
+                 uriResult.Scheme != Uri.UriSchemeHttps))
+            {
+                return new ValidationResult("Введите корректную ссылку (http/https).");
+            }
+
+            string extension = Path.GetExtension(uriResult.AbsolutePath).ToLower();
+
+            string[] allowedExtensions = { ".jpg", ".jpeg", ".png", ".webp", ".gif" };
+
+            if (!Array.Exists(allowedExtensions, e => e == extension))
+            {
+                return new ValidationResult("Ссылка должна вести на изображение (.jpg, .png, .webp и т.д.)");
+            }
+
+            return ValidationResult.Success;
+
+        }
+    }
+}
